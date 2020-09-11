73ddf5c6f366d66c70472ae8f8fb02cf6e1a2c99
Bruce Merry <bmerry@ska.ac.za>
2020-09-11 15:56:44 +0200

    Add results so far

--- collect.sh
+++ collect.sh
@@ -2,5 +2,5 @@
 set -e
 for env in venv*; do
     . $env/bin/activate
-    numactl -C0 ./httpbench.py --csv all http://localhost:8999/bucket/big-1gb.npy > results/$env-1gb.csv
+    numactl -C0 ./httpbench.py --passes 10 --csv all http://localhost:8999/bucket/big-1gb.npy | tee results/$env-1gb.csv
 done
